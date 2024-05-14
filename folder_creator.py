f39907411a263585432ce278d6fd7e1b0201f0e8
Bretzoom <deadkalter@gmail.com>
2024-05-14 13:48:06 +0400

    Try/Except integrated

--- folder_creator.py
+++ folder_creator.py
@@ -6,11 +6,17 @@ last_num = int(input('Введите номер последней папки: '
 
 if first_num <= last_num:
     while first_num <= last_num:
-        os.mkdir(f'{pattern}{first_num}')
+        try:
+            os.mkdir(f'{pattern}{first_num}')
+        except FileExistsError:
+            print(f'Директория {pattern}{first_num} уже существует.')
         first_num += 1
 
 else:
     while first_num >= last_num:
-        os.mkdir(f'{pattern}{last_num}')
+        try:
+            os.mkdir(f'{pattern}{last_num}')
+        except FileExistsError:
+            print(f'Директория {pattern}{last_num} уже существует.')
         last_num += 1
-pattern = input('Создание завершено. Нажмите ENTER, чтобы закрыть программу')
+pattern = input('Создание завершено. Нажмите ENTER, чтобы закрыть программу.')
